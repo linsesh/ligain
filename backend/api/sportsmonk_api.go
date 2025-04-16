d0b00e265d680751fc5577ee4a21cc6b28ea0b44
Benoit Linsey Fazi <benoitlinsey20@gmail.com>
2025-04-16 15:08:25 +0200

    feat: adding sportsmonk interactions (wip)

--- backend/api/sportsmonk_api.go
+++ backend/api/sportsmonk_api.go
@@ -1,21 +1,47 @@
 package api
 
-import "liguain/backend/models"
+import (
+	"fmt"
+	"liguain/backend/models"
+)
 
 // SportsmonkAPI is a wrapper around the Sportsmonk API
 type SportsmonkAPI interface {
 	// GetFixturesIdsForSeason returns all fixtures ids, for a given season, and convert to our Match model
 	GetFixturesIdsForSeason(seasonId string) (map[string]models.Match, error)
+	// GetSeasonIds creates a mapping between the season code and the season ID
+	GetSeasonIds(seasonCodes []string) (map[string]string, error)
+	// GetFixtureIds creates a mapping between the match ID and the fixture ID
+	GetFixtureIds(matches []models.Match) (map[string]string, error)
+	// GetFixturesInfos returns the fixtures infos for a given list of fixture IDs
+	GetFixturesInfos(fixtureIds []string) (map[string]models.Match, error)
 }
 
 type SportsmonkAPIImpl struct {
 	apiToken string
 }
 
+const baseURL = "https://api.sportmonks.com/v3/football/"
+const ligue1LigueId = 301
+
+var getAllFixturesLinkLigue1 = fmt.Sprintf("%sfixtures?filters=fixtureLeagues:%d", baseURL, ligue1LigueId)
+
 func NewSportsmonkAPI(apiToken string) *SportsmonkAPIImpl {
 	return &SportsmonkAPIImpl{apiToken: apiToken}
 }
 
 func (s *SportsmonkAPIImpl) GetFixturesIdsForSeason(seasonId string) (map[string]models.Match, error) {
+	return nil, nil
+}
+
+func (s *SportsmonkAPIImpl) GetSeasonIds(seasonCodes []string) (map[string]string, error) {
+	return nil, nil
+}
+
+func (s *SportsmonkAPIImpl) GetFixtureIds(matches []models.Match) (map[string]string, error) {
+	return nil, nil
+}
+
+func (s *SportsmonkAPIImpl) GetFixturesInfos(fixtureIds []string) (map[string]models.Match, error) {
 	return nil, nil
 }
--- backend/repositories/sportsmonk_repository.go
+++ backend/repositories/sportsmonk_repository.go
@@ -1,24 +1,81 @@
 package repositories
 
-import "liguain/backend/models"
+import (
+	"liguain/backend/api"
+	"liguain/backend/models"
+)
 
 type SportsmonkRepository interface {
+	// GetLastMatchInfos returns the last match infos for a given list of matches
 	GetLastMatchInfos(matches map[string]models.Match) (map[string]models.Match, error)
 }
 
 type SportsmonkRepositoryImpl struct {
 	apiToken string
+	api      api.SportsmonkAPI
 	// Fixture ID is the Sportsmonk ID for a match, we use it as a cache to avoid reconverting the match ID to a fixture ID
-	matchIdToFixtureId map[string]string
+	matchIdToFixtureId   map[string]string
+	seasonCodeToSeasonId map[string]string
 }
 
 func NewSportsmonkRepository(apiToken string) SportsmonkRepository {
 	return &SportsmonkRepositoryImpl{
-		apiToken:           apiToken,
-		matchIdToFixtureId: make(map[string]string),
+		apiToken:             apiToken,
+		api:                  api.NewSportsmonkAPI(apiToken),
+		matchIdToFixtureId:   make(map[string]string),
+		seasonCodeToSeasonId: make(map[string]string),
 	}
 }
 
 func (r *SportsmonkRepositoryImpl) GetLastMatchInfos(matches map[string]models.Match) (map[string]models.Match, error) {
-	return nil, nil
+	fixtureIds, err := r.getFixtureIds(matches)
+	if err != nil {
+		return nil, err
+	}
+	fixtureInfos, err := r.api.GetFixturesInfos(fixtureIds)
+	if err != nil {
+		return nil, err
+	}
+	return fixtureInfos, nil
+}
+
+func (r *SportsmonkRepositoryImpl) getFixtureIds(matches map[string]models.Match) ([]string, error) {
+	err := r.askAndCacheFixtureIdAndSeasonId(matches)
+	if err != nil {
+		return nil, err
+	}
+
+	fixtureIds := make([]string, 0)
+	for _, match := range matches {
+		fixtureIds = append(fixtureIds, r.matchIdToFixtureId[match.Id()])
+	}
+
+	return fixtureIds, nil
+}
+
+func (r *SportsmonkRepositoryImpl) askAndCacheFixtureIdAndSeasonId(matches map[string]models.Match) error {
+	matchesToConvert := make([]models.Match, 0)
+	matchesSeasonCodesToConvert := make([]string, 0)
+	// Get the list of matches and seasons to convert at once to reduce the number of requests
+	for _, match := range matches {
+		if _, ok := r.matchIdToFixtureId[match.Id()]; !ok {
+			matchesToConvert = append(matchesToConvert, match)
+		}
+		if _, ok := r.seasonCodeToSeasonId[match.GetSeasonCode()]; !ok {
+			matchesSeasonCodesToConvert = append(matchesSeasonCodesToConvert, match.GetSeasonCode())
+		}
+	}
+	fixtureIds, err := r.api.GetFixtureIds(matchesToConvert)
+	if err != nil {
+		return err
+	}
+	seasonsIds, err := r.api.GetSeasonIds(matchesSeasonCodesToConvert)
+	if err != nil {
+		return err
+	}
+	for _, match := range matchesToConvert {
+		r.matchIdToFixtureId[match.Id()] = fixtureIds[match.Id()]
+		r.seasonCodeToSeasonId[match.GetSeasonCode()] = seasonsIds[match.GetSeasonCode()]
+	}
+	return nil
 }
