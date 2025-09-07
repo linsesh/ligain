ea270e92e978843d26fcdc1eb24b987c9ec58821
Benoit Linsey Fazi <benoitlinsey20@gmail.com>
2025-09-07 10:06:13 +0200

    fix: still some english translations missing

--- frontend/ligain/app.config.ts
+++ frontend/ligain/app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     bundleIdentifier: 'com.ligain.app',
     supportsTablet: true,
-    buildNumber: '26',
+    buildNumber: '27',
   },
   android: {
     package: 'com.ligain.app',
